a8cb4c86ebcb0d710375dd67051c40175172a774
SArshiaK <arshia9314@gmail.com>
2024-01-01 10:46:24 +0330

    add: hash password

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -1,15 +1,21 @@
 const authServices = require('../../services/auth/auth.service');
 const {userProfileTransform} = require("../../transform/user/user.transform");
 
+const bcrypt = require('bcrypt');
+
+async function comparePassword(plaintextPassword, hash) {
+    return await bcrypt.compare(plaintextPassword, hash);
+}
+
 const registerUser = async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt();
+        req.body.password = await bcrypt.hash(req.body.password, salt);
         const {token, user} = await authServices.registerUser(req.body);
-
-        // TODO: hash password
 
         res.status(201).json({
             success: true,
-            message: 'Successfully registered .',
+            message: 'ثبت نام انجام شد',
             data: userProfileTransform(user, token)
         })
 
@@ -23,16 +29,17 @@ const registerUser = async (req, res) => {
 
 const loginByUserPass = async (req, res) => {
     try {
-        // TODO: decode hashed password
-
-        const {token, user} = await authServices.findUser({userName: req.body.userName, password: req.body.password});
+        const {token, user} = await authServices.findUser({userName: req.body.userName});
+        const result = await comparePassword(req.body.password, user.password);
+        if(!result)
+            throw 'رمز عبور اشتباه است'
 
         if(!user)
             throw 'نام کاربر ی یا کلمه عبور اشتباه است';
 
         res.status(200).json({
             success: true,
-            message: 'Successfully registered .',
+            message: 'با موفقیت وارد شدید',
             data:  userProfileTransform(user, token)
         })
 
