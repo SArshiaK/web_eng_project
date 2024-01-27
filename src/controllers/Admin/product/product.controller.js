f60902271ed756f9c8673e0cb3a96304e579c354
SArshiaK <arshia9314@gmail.com>
2024-01-27 11:41:23 +0330

    add: get products in admin

--- src/controllers/Admin/product/product.controller.js
+++ src/controllers/Admin/product/product.controller.js
@@ -3,6 +3,19 @@ const {getAllProductsTransform, productTransform} = require("../../../transform/
 
 const createProduct = async (req, res) => {
     try {
+        if(req.body?.base64){
+            const filename = `${Date.now()}.${req.body?.imgFormat ? req.body.imgFormat : 'png'}`;
+
+            const base64Data = req.body?.base64.replace(/^data:image\/png;base64,/, '');
+
+            const filePath = path.join(__dirname, `../../../../src/public/products/uploads/${filename}`);
+
+            fs.writeFileSync(filePath, base64Data, 'base64');
+
+            req.body.placeHolderImg = `uploads/${filename}`
+
+        }
+
         const product = await productService.createProduct(req.body);
 
         res.status(201).json({
@@ -12,6 +25,7 @@ const createProduct = async (req, res) => {
         })
 
     }catch (e) {
+        console.log(e.message)
         res.status(400).json({
             success: false,
             message: e
