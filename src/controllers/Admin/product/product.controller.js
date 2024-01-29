65f7c126b0c7ad99f5eff6616533de2b6d461248
SArshiaK <arshia9314@gmail.com>
2024-01-29 10:57:48 +0330

    test caprover deploy

--- src/controllers/Admin/product/product.controller.js
+++ src/controllers/Admin/product/product.controller.js
@@ -3,6 +3,7 @@ const path = require("path");
 const fs = require('fs')
 const {getAllProductsTransform, productTransform} = require("../../../transform/product/product.transform");
 
+
 const createProduct = async (req, res) => {
     try {
         if(req.body?.base64){
